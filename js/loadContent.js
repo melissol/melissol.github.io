4035a5494dce6f1fbafb8eaa9eedac34b8a8c015
Loukas <56636321+melissol@users.noreply.github.com>
2024-01-17 20:04:51 +0100

    updated code

--- js/loadContent.js
+++ js/loadContent.js
@@ -2,4 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('sections/section1.html')
         .then(response => response.text())
         .then(data => document.getElementById('section1').innerHTML = data);
+
+    fetch('sections/about.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('about').innerHTML = data);
+
+    fetch('sections/contact.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('contact').innerHTML = data);
 });
